204d367cf10d1cf26c0cc38846bcaacb2f6fa31a
yavinfour <malincayman@gmail.com>
2025-04-05 18:11:40 -0600

    added Personal page
    dsasdasdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Roulette from './components/Roulette';
-import MainContent from './components/MainContent';
-import Login from './components/Login';
-import Register from './components/Register';
-import Personal from './components/Personal';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+import Roulette from './Components/Roulette';
+import MainContent from './Components/MainContent';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Personal from './Components/Personal';
 import { DessertDataContext } from './contextsGlobal/dessertDataContext';
-import Favorites from './components/Personal';
-import Account from './components/Account';
+import Favorites from './Components/Personal';
+import Account from './Components/Account';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css';
@@ -55,13 +55,14 @@ function App() {
                   />
                 } 
               />
-              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/personal" element={<Personal />} />
               <Route path="/roulette" element={<Roulette />} />
               <Route path="/account" element={<Account />} />
               
               {/* Redirect if already logged in */}
               <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login setIsLoggedIn={setIsLoggedIn} />} />
               <Route path="/register" element={isLoggedIn ? <Navigate to="/" /> : <Register />} />
+              <Route path="/personal" element={isLoggedIn ? <Navigate to="/" /> : <Personal />} />
             </Routes>
           </div>
         </div>
