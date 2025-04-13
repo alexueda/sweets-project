f5e6cdaf137b640c35717984b364d29397e60c8f
alexueda <alex402403@gmail.com>
2025-04-13 00:20:20 -0600

    Also make the footer more slim

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
+// src/components/Footer.jsx
+import React from 'react';
 import '../css/Footer.css';
 
 function Footer() {
-
   return (
     <footer className="footer">
       <div className="footer-left">
         <h3 className="contact-us">Contact Us</h3>
-        <div>(123)875-6490</div>
+        <div className="contact-info">(123) 875-6490</div>
       </div>
-
       <div className="footer-right">
-        <div>Find Your New Favorite Treat</div>
+        <div className="slogan">Find Your New Favorite Treat</div>
       </div>
     </footer>
   );
